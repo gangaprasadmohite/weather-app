8bd1e013da2ef760295b0cf9e833300c62fbaf36
Gangaprasad Mohite <gemohite@gmail.com>
2019-11-01 22:07:19 +0530

    completed weather app

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
-
+    let temperatureDescription = document.querySelector(".temperature-description")
+    let temperatureDegree = document.querySelector(".temperature-degree")
+    let locationTimezone = document.querySelector(".location-timezone")
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
@@ -15,9 +17,19 @@ window.addEventListener('load', () => {
             })
             .then (data => {
                 console.log(data);
-                const{temperature, summary} = data.currently;
+                const{temperature, summary, icon} = data.currently;
+                temperatureDegree.textContent = temperature;
+                temperatureDescription.textContent = summary;
+                locationTimezone.textContent= data.timezone;
+                setIcons(icon, document.querySelector(".icon"))
             })
         });
         
+    }
+    function setIcons (icon, iconId) {
+        const skycons = new Skycons({color: "white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase;
+        skycons.play();
+        return skycons.set(iconId, Skycons[currentIcon]);
     }
 })
